690e5c1e33e42b39e2ece488c55bafb6dbdf8cd6
--Explosion-- <explosion.scratch@gmail.com>
2022-04-21 14:55:41 -0500

    Create Icon.svelte

---
+++ Icon.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let icon = '';
+	export let fill = 'currentColor';
+	(async () => {
+		if (!icon) {
+			return (icon = '');
+		}
+		icon = await fetch(`https://api.iconify.design/${icon}.svg?color=${encodeURIComponent(fill)}`)
+			.then((res) => res.text())
+			.catch(() => {
+				icon = '';
+			});
+		if (!icon?.startsWith('<svg')) {
+			icon = '';
+		}
+	})();
+</script>
+
+{@html icon}
