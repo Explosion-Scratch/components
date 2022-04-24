0f67ed052c8c696a1dc420c4097d0d038fc8d5a7
--Explosion-- <explosion.scratch@gmail.com>
2022-04-24 09:19:38 -0500

    Create FlexContainer.svelte

---
+++ FlexContainer.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let margin = true;
+	export let direction = 'row';
+</script>
+
+<div id="flex_container" style:margin={margin ? `6rem 0` : ''} style:flexDirection={direction}>
+	<slot />
+</div>
+
+<style>
+	#flex_container {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+	}
+</style>
